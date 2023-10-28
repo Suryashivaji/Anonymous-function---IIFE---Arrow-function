212a046ead6001674e5dae2de1d499314435b86e
Suryashivaji <devilsurya265@gmail.com>
2023-10-28 21:16:06 +0530

    Anonymous function , IIFE & Arrow function

---
+++ script.js
@@ -0,0 +1,53 @@
+// // A.Print odd numbers in an array
+
+
+var result = [];
+var a = function (arr){
+  for( var i=0; i<arr.length; i++){
+    if(a[i]%2!==0){
+      result.push(arr[i]);
+    }
+  }
+  return result;
+} 
+console.log(a([1,2,3,4,5,6,7,8]));
+
+
+var result = [];
+( function (arr){
+  for( var i=0; i<arr.length; i++){
+    if(arr[i]%2!==0){
+      result.push(arr[i]);
+    }
+  }
+  console.log(result);
+})([1,2,3,4,5,6,7,7,8,8,7,67,6,6,]) 
+
+
+
+var result=[];
+var odd =(arr)=>{
+  for( var i =0 ; i<arr.length; i++){
+    if(arr[i]%2!==0){
+result.push(arr[i]);
+    }
+  
+  }
+
+  return result;
+}
+console.log(odd([33,44,55,66,77,88]));
+
+
+
+// e.Return all the prime numbers in an array
+
+;
+
+
+
+        
+
+
+
+
